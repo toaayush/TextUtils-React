cc4fbeb56cbe65c9f7f2ca9067aa5c487ba8d093
toaayush <rajaayushays@gmail.com>
2022-10-14 21:09:09 +0530

    all comments removed

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import {
 } from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [mode, setMode] = useState('light');
   const [alert, setAlert] = useState(null);
   const showAlert = (message, type)=>{
     setAlert({
@@ -29,19 +29,11 @@ function App() {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark mode has been enabled", "success");
-      // document.title = 'TextUtils - Dark Mode';
-      // setInterval(() => {
-      //   document.title = 'TextUtils is Amazing';
-      // }, 2000);
-      // setInterval(() => {
-      //   document.title = 'Install TextUtils Now';
-      // }, 1500);
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled", "success");
-      // document.title = 'TextUtils - Light Mode';
     }
   }
 
