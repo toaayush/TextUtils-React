2274c3e9bad14348a5fd95d56eff4c394668ce27
toaayush <rajaayushays@gmail.com>
2022-10-14 08:30:58 +0530

    Updates

---
+++ precache-manifest.44cb7302052438508f306e4bdfbd9a4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "270c3e30317bf11da6f9",
+    "url": "/TextUtils-React/static/css/main.dc42817e.chunk.css"
+  },
+  {
+    "revision": "270c3e30317bf11da6f9",
+    "url": "/TextUtils-React/static/js/main.270c3e30.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtils-React/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtils-React/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "54d4ef26c5198954da45",
+    "url": "/TextUtils-React/static/js/runtime~main.54d4ef26.js"
+  },
+  {
+    "revision": "f564c32c4588b71593e0a571e698690d",
+    "url": "/TextUtils-React/index.html"
+  }
+];
